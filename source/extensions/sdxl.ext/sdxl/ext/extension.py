9cb0ca2048d0f9f0ed27e49ff3e3be2b69dd00ed
Iaaronk <92278473+Iaaronk@users.noreply.github.com>
2025-05-21 20:16:46 -0400

    Update extension.py

--- source/extensions/sdxl.ext/sdxl/ext/extension.py
+++ source/extensions/sdxl.ext/sdxl/ext/extension.py
@@ -12,50 +12,54 @@ import omni.ext
 import omni.ui as ui
 
 
-# Functions and vars are available to other extensions as usual in python:
-# `sdxl.ext.some_public_function(x)`
+# Functions and vars are available to other extensions as usual in python: `sdxl.ext.some_public_function(x)`
 def some_public_function(x: int):
-    """This is a public function that can be called from other extensions."""
     print(f"[sdxl.ext] some_public_function was called with {x}")
     return x ** x
 
 
-# Any class derived from `omni.ext.IExt` in the top level module (defined in
-# `python.modules` of `extension.toml`) will be instantiated when the extension
-# gets enabled, and `on_startup(ext_id)` will be called. Later when the
-# extension gets disabled on_shutdown() is called.
+# Any class derived from `omni.ext.IExt` in the top level module (defined in `python.modules` of `extension.toml`) will
+# be instantiated when the extension gets enabled, and `on_startup(ext_id)` will be called.
+# Later when the extension gets disabled on_shutdown() is called.
 class MyExtension(omni.ext.IExt):
-    """This extension manages a simple counter UI."""
-    # ext_id is the current extension id. It can be used with the extension
-    # manager to query additional information, like where this extension is
-    # located on the filesystem.
-    def on_startup(self, _ext_id):
-        """This is called every time the extension is activated."""
+    # ext_id is the current extension id. It can be used with the extension manager to query additional information,
+    # like where this extension is located on the filesystem.
+    def on_startup(self, ext_id):
         print("[sdxl.ext] Extension startup")
 
-        self._count = 0
-        self._window = ui.Window(
-            "SDXL Extension", width=300, height=300
-        )
+        self._window = ui.Window("SDXL Extension", width=300, height=300)
+        self.sampler_val = "K_DPM_2_ANCESTRAL"
+        def dpm_clicked():
+        	self.sampler_val = "K_DPM_2_ANCESTRAL"
+        def ddim_clicked():
+            self.sampler_val = "DDIM"
+        def lms_clicked():
+            self.sampler_val = "K_LMS"
+        def eulera_clicked():
+            self.sampler_val = "K_EULER_ANCESTRAL"
         with self._window.frame:
             with ui.VStack():
-                label = ui.Label("")
+                label = ui.Label("Enter Prompt:")
+                self.prompt_model = ui.SimpleStringModel()
+                self.prompt_field = ui.StringField(model = self.prompt_model)
+                neg_label = ui.Label("Enter Negative Prompt:")
+                self.neg_prompt_model = ui.SimpleStringModel()
+                self.neg_prompt_field = ui.StringField(model=self.neg_prompt_model)
+                with ui.HStack():
+                    collection = ui.RadioCollection()
+                    dpm_button = ui.RadioButton(radio_collection=collection, text="DPM", clicked_fn=dpm_clicked)
+                    ddim_button = ui.RadioButton(radio_collection=collection, text="DDIM", clicked_fn=ddim_clicked)
+                    lms_button = ui.RadioButton(radio_collection=collection, text="LMS", clicked_fn=lms_clicked)
+                    eulera_button = ui.RadioButton(radio_collection=collection, text="Euler A", clicked_fn=eulera_clicked)
+
+
+                self.test_label = ui.Label("Test")
 
                 def on_click():
-                    self._count += 1
-                    label.text = f"count: {self._count}"
-
-                def on_reset():
-                    self._count = 0
-                    label.text = "empty"
-
-                on_reset()
+                    self.test_label.text = self.neg_prompt_model.as_string
 
                 with ui.HStack():
                     ui.Button("Add", clicked_fn=on_click)
-                    ui.Button("Reset", clicked_fn=on_reset)
 
     def on_shutdown(self):
-        """This is called every time the extension is deactivated. It is used
-        to clean up the extension state."""
-        print("[sdxl.ext] Extension shutdown")
+        print("[sdxl.ext] Extension shutdown"
